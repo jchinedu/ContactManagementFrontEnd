ad182310733cc460a9db1163f4c2ada1fcc613cf
jchinedu <jchinedu080@gmail.com>
2025-09-21 02:57:14 +0100

    'progress'

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function clearRegisterForm() {
   document.getElementById("register-form").reset();
   document.getElementById("register-feedback").innerHTML = "";
 }
+function hideAllSections() {
+  document.querySelectorAll(".page-section").forEach(sec => sec.classList.add("hidden"));
+}
+function showHome() {
+  hideAllSections();
+  document.getElementById("home").classList.remove("hidden");
+}
+function showAbout() {
+  hideAllSections();
+  document.getElementById("about").classList.remove("hidden");
+}
+function showLogin() {
+  hideAllSections();
+  document.getElementById("login").classList.remove("hidden");
+}
+function showRegister() {
+  hideAllSections();
+  document.getElementById("register").classList.remove("hidden");
+}
+
