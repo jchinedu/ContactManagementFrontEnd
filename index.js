a95458573fab9a677de790eabd2608755de7f82f
jchinedu <jchinedu080@gmail.com>
2025-09-21 02:56:15 +0100

    'progress'

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let editingContactId = null;
 const API_URL = "http://localhost:8080/contacts";
 const AUTH_URL = "http://localhost:8080/auth";
 let authToken = null;
+function clearLoginForm() {
+  document.getElementById("login-form").reset();
+  document.getElementById("login-feedback").innerHTML = "";
+}
+function clearRegisterForm() {
+  document.getElementById("register-form").reset();
+  document.getElementById("register-feedback").innerHTML = "";
+}
